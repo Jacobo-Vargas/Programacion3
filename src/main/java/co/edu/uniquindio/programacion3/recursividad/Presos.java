2e92698f29ad3448b2b791e9f63e84d4c06d1c45
jacobo <jacobo.vargasg@uqvirtual.edu.co>
2023-09-12 13:08:01 -0500

    Ejercicios recursividad y backtracking

--- src/main/java/co/edu/uniquindio/programacion3/recursividad/Presos.java
+++ src/main/java/co/edu/uniquindio/programacion3/recursividad/Presos.java
@@ -4,43 +4,45 @@ public class Presos {
 
     public static int presos;
     public char[][] carcel = {
-            {'#', '#', '#', '#', '#', '#', '#', '#','#','#'},
-            {'#',' ', 'p', 'x', ' ', 'p', 'p', ' ', 'p','#'},
-            {'#',' ', 'p', 'p', ' ', 'p', 'p', ' ', 'p','#'},
-            {'#',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ','#'},
-            {'#','p', ' ', 'p', 'p', ' ', 'p', 'p', ' ','#'},
-            {'#','p', ' ', 'x', 'p', ' ', 'p', 'p', ' ','#'},
-            {'#',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ','#'},
-            {'#','p', 'x', 'p', ' ', 'p', 'p', ' ', 'p','#'},
-            {'#', '#', '#', '#', '#', '#', '#', '#','#','#'}
+            {' ', 'p', 'x', ' ', 'p', 'p', ' ', 'p'},
+            {' ', 'p', 'p', ' ', 'p', 'p', ' ', 'p'},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {'p', ' ', 'p', 'p', ' ', 'p', 'p', ' '},
+            {'p', ' ', 'x', 'p', ' ', 'p', 'p', ' '},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {'p', 'x', 'p', ' ', 'p', 'p', ' ', 'p',}
     };
 
     public static void main(String[] args) {
         Presos p = new Presos();
-        p.carcel[1][1] = 's';
-        p.resuelve(1, 1);
+        p.carcel[0][0] = 's';
+        p.resuelve(5, 7);
         p.verificarPresos(26);
 
     }
 
     public void resuelve(int x, int y) {
         if (ronda(x, y)) {
-            carcel[x][y] = 's';
+            carcel[x][y] = 'G';
         }
     }
 
     public boolean ronda(int i, int j) {
+
+        if (j > carcel[0].length - 1 || j < 0) {
+            return false;
+        } else if (i > carcel.length - 1 || i < 0) {
+            return false;
+        }
         if (carcel[i][j] == 'G') {
             return true;
         }
 
         if (carcel[i][j] == 'p') {
             presos += 1;
+
         }
-        if (carcel[i][j] == 'G') {
-            return true;
-        }
-        if (carcel[i][j] == '#' ||carcel[i][j]  == '*' || carcel[i][j]  == 'f') {
+        if (carcel[i][j] == '#' || carcel[i][j] == '*' || carcel[i][j] == 'f') {
             return false;
         }
 
@@ -63,15 +65,15 @@ public class Presos {
         if (result) {
             return true;
         }
-        carcel[i][j] = '*';
+        carcel[i][j] = 'f';
         return false;
     }
 
-    public void verificarPresos(int presosExistentes){
-        if(presosExistentes-presos == 0){
+    public void verificarPresos(int presosExistentes) {
+        if (presosExistentes - presos == 0) {
             System.out.println("Los presos estan completos");
-        }else{
-            System.out.println("Faltan "+(presosExistentes-presos)+" presos.");
+        } else {
+            System.out.println("Faltan " + (presosExistentes - presos) + " presos.");
         }
     }
 
