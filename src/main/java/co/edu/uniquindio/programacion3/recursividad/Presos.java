1d20e9c73b0ba71a67f2d848e72b9fe25b2b8532
jacobo <jacobo.vargasg@uqvirtual.edu.co>
2023-09-14 11:58:00 -0500

    Ejercicios recursividad y backtracking

--- src/main/java/co/edu/uniquindio/programacion3/recursividad/Presos.java
+++ src/main/java/co/edu/uniquindio/programacion3/recursividad/Presos.java
@@ -10,39 +10,47 @@ public class Presos {
             {'p', ' ', 'p', 'p', ' ', 'p', 'p', ' '},
             {'p', ' ', 'x', 'p', ' ', 'p', 'p', ' '},
             {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-            {'p', 'x', 'p', ' ', 'p', 'p', ' ', 'p',}
+            {'p', 'x', 'p', ' ', 'p', 'p', ' ', 'p'}
     };
 
     public static void main(String[] args) {
         Presos p = new Presos();
-        p.ronda(0,0);
-        //p.resuelve(5, 7);
+        p.carcel[5][7] = 'C';
+        p.carcel[0][0] = 'S';
+        p.resuelve(0, 0);
         p.verificarPresos(26);
         p.imprimirCarcel(p.carcel);
 
 
     }
 
-//    public void resuelve(int i, int j) {
-//        if(ronda(i,j)){
-//            carcel[i][j-1] = 'G';
-//            imprimirCarcel(carcel);
-//        }
-//    }
+    public void resuelve(int i, int j) {
+        if(ronda(i,j)){
+            carcel[i][j] = 'C';
+        }
+    }
 
 
 
     public boolean ronda(int i, int j) {
+        System.out.println(i+","+j);
+        // para que no se desborde
         if (j > carcel[0].length - 1 || j < 0) {
             return false;
         } else if (i > carcel.length - 1 || i < 0) {
             return false;
         }
+        if(carcel[i][j] == 'C'){
+            System.out.println("false");
+            return false;
+        }
 
         if (carcel[i][j] == 'x') {
+            carcel[i][j] = 'R';
             presos += 1;
+            return false;
 
-        } else if (carcel[i][j] == '*' || carcel[i][j] == 'p') {
+        } else if (carcel[i][j] == '*' || carcel[i][j] == 'p' || carcel[i][j] == 'R') {
             return false;
         }
 
@@ -75,7 +83,7 @@ public class Presos {
 
     public void imprimirCarcel(char[][] carcel) {
         for (int i = 0; i < carcel.length; i++) {
-            for (int j = 0; j < carcel.length; j++) {
+            for (int j = 0; j <= carcel.length; j++) {
                 System.out.print("[" + carcel[i][j] + "]");
             }
             System.out.println();
